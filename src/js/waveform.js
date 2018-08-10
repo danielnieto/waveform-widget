d5e123c25b105b7d52331d4d82737b119af12fbe
Daniel Nieto <danielnieto89@gmail.com>
2018-08-10 16:17:18 -0500

    replace double quotation marks for single ones

--- src/js/waveform.js
+++ src/js/waveform.js
@@ -1,36 +1,36 @@
-Class("Waveform").inherits(Widget)({
+Class('Waveform').inherits(Widget)({
     ELEMENT_CLASS: 'waveform',
     prototype: {
-        progress: 0,
-        svgIsLoaded: false,
+        _progress: 0,
+        _svgIsLoaded: false,
         init: function(config) {
             Widget.prototype.init.call(this, config);
 
             var waveform = this;
             var svgFile = config.svg[config.svgToUse];
 
-            if(config.name){
-                waveform.element.append(
-                    '<div class="title">' + config.name + "</div>"
-                );
+            if (config.name) {
+                waveform.element.append('<div class="title">' + config.name + '</div>');
             }
-            waveform.$svgContainer = $("<div class='svg-container'></div>");
+
+            waveform.$svgContainer = $('<div class="svg-container"></div>');
             waveform.element.append(waveform.$svgContainer);
 
-            waveform.$svgContainer.load(svgFile, function( response, status, xhr ) {
-                if (status === "success") {
-                    waveform.svgIsLoaded = true;
-                    waveform.$progress = waveform.$svgContainer.find(".progress");
+            waveform.$svgContainer.load(svgFile, function(response, status, xhr) {
+                if (status === 'success') {
+                    waveform._svgIsLoaded = true;
+                    waveform.$progress = waveform.$svgContainer.find('.progress');
                     waveform._updateProgressBar();
                 }
             });
         },
         _updateProgressBar: function _updateProgressBar() {
-
-            if(this.svgIsLoaded){
+            if (this._svgIsLoaded) {
                 this.$progress.css('width', this.progress + '%');
             }
-
+        },
+        isSVGLoaded: function isSVGLoaded() {
+            return this._svgIsLoaded;
         },
         getProgress: function getProgress() {
             return this.progress;
